93ea4510816766d4fb632e79a1b5f6b3c7d9a51a
Vitaliy Pugachev <pugachev.vt@gmail.com>
2024-04-14 18:41:06 +0300

    add headers

--- src/services/getEvents.js
+++ src/services/getEvents.js
@@ -1,7 +1,12 @@
 const baseUrl = 'http://5.35.88.152:6000/';
 
 export const getEvents = async (id) => {
-    const response = await fetch(baseUrl + id);
+    const response = await fetch(baseUrl + id, {
+        headers: {
+            'Accept': '*/*',
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+    });
 
     if (!response.ok) {
         throw Error('Data fetching error!');
