63e7184168c9cc8ad11174cf4f7b65a56a4dd281
Vitaliy Pugachev <pugachev.vt@gmail.com>
2024-04-14 19:00:25 +0300

    reser config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    proxy: 'http://5.35.88.152:6000'
-  }
+  // devServer: {
+  //   proxy: 'http://5.35.88.152:6000'
+  // }
 })
